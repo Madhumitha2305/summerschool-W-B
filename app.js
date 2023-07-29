92f2f3a4dc5f7eda2b307901a219a61ad0b21051
Madhumitha2305 <124774038+Madhumitha2305@users.noreply.github.com>
2023-07-30 00:51:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,21 @@
 // app.js
-const contractAddress = '0x27c9cCF9824f1930363e75c944FBDA58Be3F8818'; // Replace this with your smart contract address
-const contractABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "set",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "get",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "storedData",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-]; // Replace this with your smart contract ABI
+const contractAddress = ''; 
+const contractABI = []
 
 async function connectMetamask() {
-    // Modern browsers with MetaMask installed
     if (window.ethereum) {
         try {
-            // Request account access if needed
             await window.ethereum.request({ method: 'eth_requestAccounts' });
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             const signer = provider.getSigner();
             const contract = new ethers.Contract(contractAddress, contractABI, signer);
             return contract;
         } catch (error) {
-            console.error('Error connecting to MetaMask:', error);
+            console.error('Error', error);
             return null;
         }
     } else {
-        console.error('MetaMask extension not detected.');
+        console.error('MetaMask error');
         return null;
     }
 }
